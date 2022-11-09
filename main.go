02867cbad46f91d1c67918d00fdb27fd23ee33c8
kartikchauhan <chauhan.kartik25@gmail.com>
2022-11-09 16:36:38 +0530

    add flag complete

--- main.go
+++ main.go
@@ -14,19 +14,27 @@ var (
 
 func main() {
 	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "complete a task")
 
 	flag.Parse()
 
 	todos := &taskmaster.Todos{}
+
 	if err := todos.Load(filename); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
 	switch {
 	case *add:
 		todos.Add("make an app")
-		todos.Save(filename)
+		err := todos.Save(filename)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		todos.Complete(*complete)
 	default:
 		fmt.Fprintln(os.Stderr, "Invalid option")
 	}
